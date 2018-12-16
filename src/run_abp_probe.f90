ac08fb2f29bb8fe6602bc0a41d2dbc253d2d17a3
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-12-16 21:52:55 +0100

    use config

--- src/run_abp_probe.f90
+++ src/run_abp_probe.f90
@@ -1,24 +1,27 @@
 program run_abp_probe
   use common
+  use config_m
   use abp_model_m
   implicit none
 
   integer :: N
 
   type(abp_t) :: abp
+  type(config_t) :: conf
 
-  integer :: i
-  real(kind=rk) :: dt
+  integer :: i, n_therm, n_loop, n_steps
+  real(kind=rk) :: dt, probe_T
 
   integer :: pos_unit, theta_unit, vel_unit
   character(len=:), allocatable :: coord_format, theta_format
   real(kind=rk) :: phi, sigma_abp, sigma_probe, l
 
+  call conf%init(filename=get_character_argument(1))
 
-  sigma_abp = 0.5_rk
-  sigma_probe = 4.5_rk
-  l = 30
-  phi = 0.3_rk
+  sigma_abp = conf%get_d('sigma')
+  sigma_probe = conf%get_d('sigma_probe')
+  l = conf%get_d('box_l')
+  phi = conf%get_d('phi')
 
   N = 1 + floor((phi*l**2 - pi*sigma_probe**2) / (pi*sigma_abp**2))
 
@@ -42,46 +45,47 @@ program run_abp_probe
 
   call abp%init(N, L=[l, l])
 
-  abp%v0 = 1._rk
-  abp%Dr = 0.1_rk
-  abp%D = 0.1_rk
+  abp%v0 = conf%get_d('v0')
+  abp%Dr = conf%get_d('Dr')
+  abp%D = conf%get_d('D')
   abp%mu = 1/abp%D
   abp%sigma = sigma_abp
   abp%epsilon = 1
 
+  dt = conf%get_d('dt')
+  n_therm = conf%get_i('n_therm')
+  n_loop = conf%get_i('n_loop')
+  n_steps = conf%get_i('n_steps')
+
   ! no noise and no self-prop for probe
-  abp%D(1) = 0
+  probe_T = conf%get_d('probe_T')
+  abp%D(1) = probe_T*abp%D(1)*sigma_abp/sigma_probe
   abp%v0(1) = 0
   abp%mu(1) = abp%mu(1)*sigma_abp/sigma_probe
   abp%sigma(1) = sigma_probe
 
   call abp%random_placement()
 
-  dt = 0.00001_rk
-
-  do i = 1, 100
+  do i = 1, n_therm
 
      if (modulo(i, 100)==0) &
           write(*,*) 'thermalization', i
 
-     call abp%srk_step(100, dt)
+     call abp%srk_step(100, dt/10)
 
   end do
 
-  dt = 0.001_rk
-
-  do i = 1, 5000
+  do i = 1, n_loop
 
      if (modulo(i, 100)==0) &
           write(*,*) 'integration', i
 
-     call abp%srk_step(500, dt)
+     call abp%srk_step(n_steps, dt)
 
      write(pos_unit, coord_format) abp%x
      write(vel_unit, coord_format) abp%v
      write(theta_unit, theta_format) abp%theta
 
   end do
-
 
 end program run_abp_probe
